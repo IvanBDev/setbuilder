b7b0f31fcfd8cb73d9bed4ca671825ea4d95c9ea
IvanBDev <ivan_bendotti@yahoo.com>
2025-06-11 11:21:48 +0200

    First commit

---
+++ src/test/java/it/calamity/setbuilder/SetbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.calamity.setbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetbuilderApplicationTests {
+    
+    @Test
+    void contextLoads ( ) {
+    }
+    
+}
